36f3185d0144d82c00ee5ff7541cb9667c89c26c
Advitiya Sood <practice@gmail.com>
2025-05-12 09:43:49 +0530

    SQL database used

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:   " PASSWORD WITHOUT SPECIAL CHAR "  @localhost/todo_db'
 db=SQLAlchemy(app)
 
 
